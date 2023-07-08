78fad9dcfb9ba2fb7434a0f2188a676d5b56eeda
owaisdurvesh <owaisdurvesh@gmail.com>
2023-07-08 23:26:50 +0500

    Store-App-react-native

---
+++ App.tsx
@@ -0,0 +1,15 @@
+// import Signup from "./Screens/Signup"
+import Navigator from "./navigation/navigation";
+// import Login from"./Screens/Login"
+
+function App(){
+  return(
+    <>
+    <Navigator />
+    {/* <Signup/>
+    <Login/> */}
+    
+    </>
+  )
+}
+export default App
